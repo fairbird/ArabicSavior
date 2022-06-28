57b9c491fc4f77fff7fd6e3b5de1bfa89bd4ef72
RAED <rrrr53@hotmail.com>
2022-06-29 00:36:28 +0300

    Bump V 1.9
    - Move plugin to my source github

--- usr/lib/enigma2/python/Plugins/Extensions/ArabicSavior/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/ArabicSavior/plugin.py
@@ -250,7 +250,8 @@ class ArabicSaviorSetup(ConfigListScreen, Screen):
         def checkupdates(self):
                 try:
                         from twisted.web.client import getPage, error
-                        url = b"http://tunisia-dreambox.info/TSplugins/ArabicSavior/installer.sh" 
+                        #url = b"http://tunisia-dreambox.info/TSplugins/ArabicSavior/installer.sh"
+                        url = b"https://github.com/fairbird/ArabicSavior/blob/fairbird/installer.sh"
                         getPage(url,timeout=10).addCallback(self.parseData).addErrback(self.errBack)
                 except Exception as error:
                         trace_error()
@@ -280,7 +281,8 @@ class ArabicSaviorSetup(ConfigListScreen, Screen):
                 try:
                         if answer:
                                 cmdlist = []
-                                cmd="wget http://tunisia-dreambox.info/TSplugins/ArabicSavior/installer.sh -O - | /bin/sh"
+                                #cmd="wget http://tunisia-dreambox.info/TSplugins/ArabicSavior/installer.sh -O - | /bin/sh"
+                                cmd="wget https://github.com/fairbird/ArabicSavior/blob/fairbird/installer.sh -O - | /bin/sh"
                                 cmdlist.append(cmd)
                                 self.session.open(Console, title='Installing last update, enigma will be started after install', cmdlist=cmdlist, finishedCallback=self.myCallback, closeOnSuccess=False)
                 except:
