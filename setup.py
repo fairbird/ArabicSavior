e6f68f7496ab8797887aadcec6d575e4085c84c5
RAED <rrrr53@hotmail.com>
2022-07-02 13:02:02 +0300

    [setup.py] Update

--- setup.py
+++ setup.py
@@ -12,5 +12,4 @@ setup(name='enigma2-plugin-extensions-ArabicSavior',
        packages=[PLUGIN_DIR],
        package_dir={PLUGIN_DIR: 'usr'},
        package_data={PLUGIN_DIR: ['plugin.png', '*/*.png']},
-       package_data={PLUGIN_DIR: ['*.png', 'images/*.png', 'images/preview/*.png', 'locale/*/LC_MESSAGES/*.mo']},
       )
